b2858c2918e135a984e8233b2542a71ae75005b2
John Tuttle <thejwt@gmail.com>
2017-10-17 01:57:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,7 +69,7 @@ class Student
   end
 
   def update
-    sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
+    sql = "UPDATE songs SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
